b3e1cd3fc207dc6c02d49cfda96ed7003ffc6000
Tiago Nascimento <tiago.nascimento@spruceid.com>
2021-08-28 17:50:00 -0300

    Initial commit
    Signed-off-by: Tiago Nascimento <tiago.nascimento@spruceid.com>

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use rocket::form::{Form, FromForm};
+use rocket::fs::{relative, FileServer};
+use rocket::{launch, post, routes};
+use sha2::{Digest, Sha512};
+use std::collections::HashMap;
+
+const URL: &str = env!("HS_URL", "missing HS_URL env variable");
+
+#[derive(FromForm)]
+struct SignInForm {
+    user: String,
+    pass: String,
+}
+
+#[post("/sign-in", data = "<form>")]
+async fn sign_in(form: Form<SignInForm>) -> &'static str {
+    let SignInForm { user, pass } = form.into_inner();
+    let data = format!("{}{}", pass, user.repeat(10));
+    let digest = Sha512::digest(data.as_bytes());
+    let pass = hex::encode(digest);
+    println!("pass={}", pass);
+
+    let mut params = HashMap::new();
+    params.insert("user", user);
+    params.insert("pass", pass);
+
+    let client = reqwest::Client::new();
+    client.post(URL).form(&params).send().await;
+
+    "Se a senha estiver correta, você ouvirá um som e a porta abrirá"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![sign_in])
+        .mount("/", FileServer::from(relative!("static")))
+}
